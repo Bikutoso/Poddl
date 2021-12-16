7cb3fff605a0fdf7708f435a64871706bcabb90a
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2021-12-16 15:24:55 +0100

    Created interactive mode

--- lib/poddl/input/interactive.rb
+++ lib/poddl/input/interactive.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative "../word"
 require_relative "common"
 
 module Poddl
@@ -7,6 +8,28 @@ module Poddl
     # @todo Create class
     class Interactive
       include Poddl::Input::Common
+
+      # Initialize instance with specified arguemtn options
+      # 
+      # @param options [Poddl::Options] parsed optparse object
+      def initialize(options)
+        @options = options
+      end
+
+      # Query use for word then calls {Poddl::Input::Handler#get} on it.
+      #
+      # @return [Boolean] return value
+      # @see Poddl::Input::Handler#run
+      def run
+        puts "Interactive mode"
+        kana = gets("Enter kana: ").chomp
+        kanji = gets("Enter kanji: ").chomp
+        
+        kanji = nil if kanji.empty?
+
+        word = Poddl::Word.new([kana, kanji])
+        get(word)
+      end
     end
   end
 end
