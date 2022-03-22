4c5d053c20f9a4116aa84a57e425f6395b34d286
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2022-03-22 14:33:20 +0100

    Moved csv parsing to Input::File; Moved threads to its own method

--- lib/poddl/input/file.rb
+++ lib/poddl/input/file.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "../parse_csv"
+require "csv"
+
+require_relative "../logger"
 
 module Poddl
   module Input
@@ -8,6 +10,8 @@ module Poddl
     class File
       # NOTE: Common is removed as we handle the downloading manually
       # Are there ways we move parts of it into common?
+
+      include Logging
 
       # Initialize instance with specified argument options
       # @param options [Poddl::Options] parsed optparse object
@@ -21,9 +25,24 @@ module Poddl
       def run
         # Create and fill queue
         q = Queue.new
-        ParseCSV.parse(@options.input_file).each { |word| q << word }
+        generate_words(@options.input_file).each { |word| q << word }
 
         dw = Poddl::Downloader.new(@options)
+
+        thread_download(q, dw)
+
+        q.close # Necessary to close?
+        # TODO: This currently always successfull. Make it return when failed
+        0
+      end
+
+      private
+
+      # Starts threads with downloader
+      # @param q [Queue] work queue
+      # @param q [Poddl:Downloader] downloader
+      # @return [Boolean] success?
+      def thread_download(q, dw)
         threads = []
         @options.threads.times do
           threads << Thread.new do
@@ -31,11 +50,21 @@ module Poddl
             dw.download(q.pop, @options.save_path) until q.empty?
           end
         end
-
         threads.each(&:join)
-        q.close # Necessary to close?
-        # TODO: This currently always successfull. Make it return when failed
         0
+      end
+
+      # Parses a CSV file from file
+      # @param path [String] CSV file to parse
+      # @return [Array<Poddl:Word>] array of words
+      def generate_words(path)
+        words = []
+        CSV.read(path).each do |word|
+          words.append(Poddl::Word.new(word))
+          logger.debug "Generated word:#{word}, #{words[-1]}"
+        end
+        # I feel there is a way to use the each loop instead of mentioning the array here.
+        words
       end
     end
   end
